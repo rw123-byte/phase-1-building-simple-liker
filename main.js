ba9ee89c8b95ae9737e7d794ab771342e8e4f64f
ruthwekesa <wekesaruth569@gmail.com>
2022-08-27 21:41:40 +0300

    Assignment completed

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-    // Your JavaScript code goes here!
 const articleHearts = document.querySelectorAll(".like-glyph");
 
 function likeCallback(e) {
@@ -27,24 +26,6 @@ function likeCallback(e) {
 for (const glyph of articleHearts) {
     glyph.addEventListener("click", likeCallback);
 }
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
-
-function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(function() {
-            let isRandomFailure = Math.random() < .2
-            if (isRandomFailure) {
-                reject("Random server error. Try again.");
-            } else {
-                resolve("Pretend remote server notified of action!");
-            }
-        }, 300);
-    });
-}
-
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
